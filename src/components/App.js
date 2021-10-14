852c32bce113d9b475a4d769e0b159b7bbb150c4
Laurence Floriani <laurenceflo@hotmail.com>
2021-10-14 18:53:07 +0200

    add menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import Banner from "./Banner.js";
 import Polytopes from "./Polytopes.js";
 import Graphs from "./Graphs.js";
+import Menu from "./Menu.js";
 
 function App() {
     return (
         <div className="app">
             <header className="app_header">
                 <Banner />
+                <Menu />
                 <Polytopes />
                 <Graphs />
             </header>
---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Menu extends React.Component {
+    render() {
+        return (
+            <div className="menu">
+                <h2>Menu</h2>
+            </div>
+        )
+    }
+}
+
+export default Menu;
