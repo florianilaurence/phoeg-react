b23209becc22dc36640627eec2a76d84e9986acd
FlorianiLaurence <laurenceflo@hotmail.com>
2021-11-20 22:08:58 +0100

    change to tsx

--- src/components/PolytopeChart.tsx
+++ src/components/PolytopeChart.tsx
@@ -1,8 +1,9 @@
 import {readEnvelope, readPoints} from "../core/ParseFiles";
-import React, {useEffect, useState} from "react";
+import * as React from "react";
+import {useEffect, useState} from "react";
 import {SigmaContainer, useSigma} from "react-sigma-v2";
 import Graph from "graphology";
-import Sigma from "sigma";
+import Sigma  from "sigma";
 
 export default function PolytopeChart(props) {
     const [envelope, setEnvelope] = useState([{x: 0, y: 0}, {x: 1, y: 1}]);
@@ -12,7 +13,7 @@ export default function PolytopeChart(props) {
     const update = () => {
         let pathEnv = "assets/data_" + props.invariant + "/enveloppes/enveloppe-" + props.number + ".json";
         let pathPoints = "assets/data_" + props.invariant + "/points/points-" + props.number + ".json";
-
+/*
         fetch(pathPoints,{headers : {'Content-Type': 'application/json', 'Accept': 'application/json'}})
             .then(function(response){
                 return response.json();
@@ -20,7 +21,7 @@ export default function PolytopeChart(props) {
             .then(function(myJson) {
                 setPoints(readPoints(myJson, props.invariant));
             });
-
+*/
         fetch(pathEnv, {headers : {'Content-Type': 'application/json', 'Accept': 'application/json'}})
             .then(function(response){
                 return response.json();
@@ -34,9 +35,6 @@ export default function PolytopeChart(props) {
     const MyCustomGraph = () => {
         const sigma = useSigma();
         const graph = sigma.getGraph();
-        graph.addNode("Jessica", { label: "Jessica", x: 1, y: 1, color: "#FF0", size: 10 });
-        graph.addNode("Truman", { label: "Truman", x: 0, y: 0, color: "#00F", size: 5 });
-        graph.addEdge("Jessica", "Truman", { color: "#CCC", size: 1 });
         return null;
     }
 
@@ -48,14 +46,11 @@ export default function PolytopeChart(props) {
             <p>
                 Invariant : {props.invariant} Color : {props.color} Number : {props.number}
             </p>
-            ReactDOM.render(
                 <React.StrictMode>
                     <SigmaContainer style={{ height: "500px", width: "500px" }}>
                         <MyCustomGraph />
                     </SigmaContainer>
-                </React.StrictMode>,
-            document.getElementById("root"),.
-            );
+                </React.StrictMode>
         </div>
     )
 
