a09ad7c2e8c235dfa99877ffbc0a60af75bd0524
FlorianiLaurence <laurenceflo@hotmail.com>
2021-11-21 21:51:20 +0100

    debug parse envelope

--- src/components/PolytopeChart.js
+++ src/components/PolytopeChart.js
@@ -1,47 +1,34 @@
-import $ from "jquery";
 import {readEnvelope, readPoints} from "../core/ParseFiles";
 import React, {useEffect, useState} from "react";
 import {Bubble} from "react-chartjs-2";
 
 export default function PolytopeChart(props) {
-    const [envelope, setEnvelope] = useState([{x: 0, y: 0}, {x: 1, y: 1}]);
-    const [points, setPoints] = useState([{data: [{x: 0, y: 0, r: 5}]}]); // Remarque j'ai besion ici d'une liste de datasets car les points doivent avoir des couleurs différentes
-    const [data, setData] = useState({datasets: [points, {type: 'line', data: envelope}]})
+    const [data, setData] = useState({datasets: [
+            {type: 'bubble', data: [{x: 0.5, y: 0.5, r: 5}, {x: 0.25, y: 0.25, r: 10}]},
+            {type: 'line', data: [{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 1, y: 0}, {x: 0, y: 0}]}
+        ]})
 
-    const update = () => {
+    useEffect( () => {
         let pathEnv = "assets/data_" + props.invariant + "/enveloppes/enveloppe-" + props.number + ".json";
         let pathPoints = "assets/data_" + props.invariant + "/points/points-" + props.number + ".json";
 
-        fetch(pathPoints
-            ,{
-            headers : {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }})
+        fetch(pathEnv,{headers : {'Content-Type': 'application/json', 'Accept': 'application/json'}})
             .then(function(response){
                 return response.json();
             })
             .then(function(myJson) {
-                setPoints(readPoints(myJson, props.invariant));
+                const envelope = readEnvelope(myJson);
+                fetch(pathPoints,{headers : {'Content-Type': 'application/json', 'Accept': 'application/json'}})
+                    .then(function(response){
+                        return response.json();
+                    })
+                    .then(function(myJson) {
+                        const points = readPoints(myJson, props.invariant);
+                        setData({datasets: [{type: 'bubble', data: points}, {type: 'line', data: envelope}]});
+                    });
             });
-
-
-        fetch(pathEnv
-            ,{
-            headers : {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }})
-            .then(function(response){
-                return response.json();
-            })
-            .then(function(myJson) {
-                setEnvelope(readEnvelope(myJson));
-            });
-        setData({datasets: [points, {type: 'line', data: envelope}]});
     }
-
-    useEffect(update, [props.invariant, props.color, props.number]);
+    , [props.invariant, props.color, props.number]);
 
     return (
         <div>
