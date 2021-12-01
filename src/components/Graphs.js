f604488c161b7e25067f6049fae4b4a25fd17674
FlorianiLaurence <laurenceflo@hotmail.com>
2021-12-01 11:48:40 +0100

    add parse signature (not work)

--- src/components/Graphs.js
+++ src/components/Graphs.js
@@ -4,6 +4,31 @@ import Select from "react-select";
 import { Sigma, RandomizeNodePositions, RelativeSize } from "react-sigma";
 
 
+function unpack(str) {
+    let bytes = [];
+    for (let i = 0; i < str.length; i++) {
+        let char = str.charCodeAt(i);
+        //bytes.push(char >>> 8);
+        bytes.push(char & 0xFF);
+        }
+    return bytes;
+}
+
+function bytesArrayToN(bytesArray) {
+    if (bytesArray[0] <= 62)
+        return [bytesArray[0], bytesArray.slice(1)];
+
+    if (bytesArray[1] <= 62)
+        return [(bytesArray[1] << 12) + (bytesArray[2] << 6) + bytesArray[3], bytesArray.slice(4)];
+
+    return [(bytesArray[2] << 30)
+                + (bytesArray[3] << 24)
+                + (bytesArray[4] << 18)
+                + (bytesArray[5] << 12)
+                + (bytesArray[6] << 6)
+                + bytesArray[7],
+        bytesArray.slice(8)];
+}
 
 export default function Graphs(props) {
     const [graphs, setGraphs] = useState([]); // La liste des graphes correspondant aux critères
@@ -33,7 +58,44 @@ export default function Graphs(props) {
     }
 
     const constructGraph = () => {
+        let signature = currentGraph["sig"];
+        const bytesArr = unpack(signature);
+        for (let i in bytesArr) {
+            bytesArr[i] -= 63;
+        }
+        let [n, data] = bytesArrayToN(bytesArr);
+        let nd = Math.floor((Math.floor(n * (n - 1) / 2) + 5) / 6)
+        let bits = []
+        for (let b of data) {
+            for (let i = 5; i >= 0; i--) {
+                bits.push((b >> i) & 1);
+            }
+        }
+        const nodes = [];
+        for (let i = 0; i < n; i++) {
+            nodes.push({
+                data: {
+                    id: `n${i}`
+                }
+            });
+        }
 
+        const edges = [];
+        let cnt = 0;
+        let edgesCnt = 0;
+        for (let j = 1; j < n; j++) {
+            for (let i = 0; i < j; i++) {
+                if (bits[cnt]) {
+                    edges.push({
+                        data: { id: `e${edgesCnt}`, source: `n${i}`, target: `n${j}` }
+                    })
+                    edgesCnt += 1;
+                }
+                cnt += 1;
+            }
+        }
+        console.log(bytesArr);
+        return { nodes, edges };
     }
 
     return (
