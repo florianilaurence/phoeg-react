34661a8f76ddb53f9f1e97963961b61cd5b44dbf
FlorianiLaurence <laurenceflo@hotmail.com>
2021-12-18 21:19:18 +0100

    Modify domain for x axis

--- src/components/PolytopeChart.js
+++ src/components/PolytopeChart.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {Axis, GlyphSeries, LineSeries, XYChart} from "@visx/xychart";
 import {readEnvelope, readPoints} from "../core/ParseFiles";
+import { Zoom } from "@visx/zoom";
 
 const accessors = (data, param) => {
     if (data !== undefined) { // Obligatoire sinon problème car est parfois appelé avec un undefined
@@ -22,9 +23,10 @@ export default function PolytopeChart(props) {
     const [lines, setLines] = useState([{}]);
     const [maxCluster, setMaxCluster] = useState(10);
     const [clusterNb, setClusterNb] = useState(1);
+    const [domainX, setDomainX] = useState([1, 10]);
 
-    const xScale = {type: 'linear'}
-    const yScale = {type: 'linear'}
+    const xScale = {type: 'linear', domain: domainX};
+    const yScale = {type: 'linear'};
 
     useEffect( () => {
         let pathEnv = "assets/data_" + props.invariantName + "/enveloppes/enveloppe-" + props.numberVertices + ".json";
@@ -71,14 +73,19 @@ export default function PolytopeChart(props) {
 
     const regroupedPoints = (points) => {
         let pointsGr = {};
+        let minX = 100;
+        let maxX = 0;
         for (let point of points) {
             if (pointsGr[point.col] == null) {
                 pointsGr[point.col] = [];
             }
+            minX = Math.min(minX, point.x);
+            maxX = Math.max(maxX, point.x);
             pointsGr[point.col].push(point);
         }
         const colors = Object.keys(pointsGr).map(x => parseInt(x)).sort((a, b) => a >= b);
         setMaxCluster(colors.length);
+        setDomainX([Math.floor(minX), Math.ceil(maxX)]);
         if (colors.length > clusterNb) {
             return regroupedPoints2(pointsGr, colors);
         } else {
@@ -97,9 +104,11 @@ export default function PolytopeChart(props) {
                 <GlyphSeries
                     dataKey={`${group[0].col} - ${group[group.length-1].col}`}
                     data={group}
+                    size={4}
                     xAccessor={data => accessors(data, 'x')}
                     yAccessor={data => accessors(data, 'y')}
-                    renderGlyphs={data => accessors(data, 'r')} />
+                    renderGlyphs={data => accessors(data, 'r')}
+                />
             )
         }
         return result;
@@ -111,8 +120,8 @@ export default function PolytopeChart(props) {
                 height={500}
                 xScale={xScale}
                 yScale={yScale}>
-                <Axis orientation="bottom" />
-                <Axis orientation="left" />
+                <Axis orientation="bottom" label={props.invariantName}  />
+                <Axis orientation="left" label="Nombres d'arêtes"/>
                 <LineSeries
                     dataKey="Enveloppe"
                     data={lines}
@@ -120,13 +129,16 @@ export default function PolytopeChart(props) {
                     yAccessor={data => accessors(data, 'y')} />
                 <RenderGlypheSeries />
             </XYChart>
-            <p>Combien souhaitez-vous de clusters pour colorier les graphes ? Entre {1} et {maxCluster}.</p>
+            <p>
+                Combien souhaitez-vous de clusters pour colorier les graphes ? Entre {1} et {maxCluster}.
+            </p>
             <button onClick={() => clusterNb > 1 ? setClusterNb(clusterNb - 1):setClusterNb(1)}> - </button>
             {" " + clusterNb + " "}
             <button onClick={() => setClusterNb(Math.max((clusterNb + 1) % (maxCluster + 1), 1))}> + </button>
             {
                 // Attention le changement du nombre de clusters n'entraîne pas toujours un changement
                 // Car au moment de au ceil, on peut tomber plusieurs fois sur la même valeur
+                // Math.ceil(3.3) === Math.ceil(3.6)
             }
         </div>
     );
