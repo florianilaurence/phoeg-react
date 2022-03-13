32b8e2313c1f5915dbdab8b8f1bbdf5d598cb404
FlorianiLaurence <laurenceflo@hotmail.com>
2022-03-13 16:27:22 +0100

    Begin add style with mui material

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,41 @@
-import Banner from "./Banner.js";
 import Polytopes from "./Polytopes.js";
 import Menu from "./Menu.js";
+import "react-banner/dist/style.css";
+import {AppBar, Box, IconButton, Toolbar} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { faInfo } from '@fortawesome/free-solid-svg-icons/faInfo';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 // Component's core
 export default function App() {
     return (
         <div className="app">
-            <header className="app_header">
-                <Banner/>
-                <Menu/>
-                <Polytopes/>
-            </header>
+            <Box sx={{flexGrow: 1}}>
+                <AppBar position="static" style={{ background: '#000000'}}>
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{mr: 2}}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                            News
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            TODO : Ajouter Titre + Logo !!!
+            <Button variant="contained" startIcon={<FontAwesomeIcon icon={faInfo} />} >
+                Tuto
+            </Button>
+            <Polytopes/>
         </div>
     )
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import {useState} from "react";
+
+
+export default function NavBar() {
+    const [state, setState] = useState({ isOpen: false });
+    let toggle = () => setState({ isOpen: !this.state.isOpen })
+
+    return (
+        <div className="mobile-wrapper" >
+
+        </div>
+    )
+
+}
