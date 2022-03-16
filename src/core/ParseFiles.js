c942d940a21b7c6635e6d22bffec045d6b5e3394
Thomas Lavend'Homme <lavendthomas@outlook.be>
2022-03-16 08:42:28 +0100

    Fix showing graphs by updating ParseFiles.js

--- src/core/ParseFiles.js
+++ src/core/ParseFiles.js
@@ -54,6 +54,7 @@ export function readPoints(data, invariantX, invariantY, invariantColor) {
     return pointsGrouped;
 }
 
+
 /**
  *
  * @param data Données du Json
@@ -63,12 +64,21 @@ export function readPoints(data, invariantX, invariantY, invariantColor) {
  * @returns {*[]} Liste des signatures de graphes correspondant aux critères
  */
 
-export function readGraph(data, numberEdges, invariantName, invariantValue) {
-    let result =[];
-    for (let d of data) {
-        if (d["m"] === numberEdges && d[invariantName] === invariantValue) {
-            result.push(d["sig"]);
+export function readGraph(data, x_invariant_name, x_invariant_value, y_invariant_name, y_invariant_value) {
+    const result =[];
+    const keys = Object.keys(data);
+    const invariants_length = data[keys[0]].length;
+    console.assert(keys.every(key => data[key].length === invariants_length), "Assert all invariants must have the same length");
+
+    for (let i = 0; i<invariants_length; i++) {
+        const xValue = data[x_invariant_name][i];
+        const yValue = data[y_invariant_name][i];
+        const sigValue = data["sig"][i];
+
+        if (xValue === x_invariant_value && yValue === y_invariant_value) {
+            result.push(sigValue);
         }
     }
+
     return result;
 }
